e12966673773d764989342e734ad414f84b77540
LaurenJanicki <lauren.a.janicki@gmail.com>
2015-07-03 12:22:04 -0700

    Completed Tree!!!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,24 +3,36 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+  var result = false;
+  var searchTree = function(node) {
+    if (node.value === target) {
+      result = true;
+    } else {
+      for(var i = 0; i < node.children.length; i++) {
+        searchTree(node.children[i]);
+      }
+      // _.each(node.children, function(child){ 
+      //   console.log(searchTree(child));
+      //   return searchTree(child);
+      // });
+    }
+    return result;
+  };
+return searchTree.call(this, this);
 };
+
 
 
 /*
