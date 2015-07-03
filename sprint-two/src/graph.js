466cdc453c068804b8d32f275cf528a6c6e7ec20
LaurenJanicki <lauren.a.janicki@gmail.com>
2015-07-03 16:42:39 -0700

    Draft of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,30 +1,42 @@
 
 
 var Graph = function(){
-
 };
 
 Graph.prototype.addNode = function(node){
-  this.node = node;
+  this[node] = {}
+  this[node].edges = [];
 };
 
 Graph.prototype.contains = function(node){
-  for(key in this){
-    if(this[key] === node){
+  for(var key in this){
+    if (key === node) {
       return true;
     } else {
       return false;
-    }
+    } 
   }
 };
 
 Graph.prototype.removeNode = function(node){
+  for(var key in this) {
+    if(key === node) {
+      delete this[key];
+    }
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  if(_.contains(this[fromNode].edges, toNode)){
+     return true;
+   } else {
+     return false;
+   }
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this[fromNode].edges.push(toNode);
+  this[toNode].edges.push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
