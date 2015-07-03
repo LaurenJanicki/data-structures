d46fec114ab348e2fad61bc5e91d43589222835e
LaurenJanicki <lauren.a.janicki@gmail.com>
2015-07-02 20:59:26 -0700

    Completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,43 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var counter = 0;
 
   list.addToTail = function(value){
+    if(!list.tail){
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head;
+    //delete list.head;
+    list.head = list.head.next;
+    return temp.value;
   };
 
   list.contains = function(target){
+    // Set head to current
+    var checkValue = function(node) {
+    // Check if node value matches target
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return checkValue(node.next);
+      }
+      // If yes, return true
+      // If no, check current.next
+    // Base case: if current.next === null return false
+    };
+
+    //return boolean
+    return checkValue(list.head);
   };
 
   return list;
