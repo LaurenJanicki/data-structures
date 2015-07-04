91d6cd750a989cea9587de7ed72e53c46c183f2d
LaurenJanicki <lauren.a.janicki@gmail.com>
2015-07-04 09:55:43 -0700

    Completed Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+	return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item){
+  _.each(this._storage, function(value, index, collection){
+    if (value === item) {
+      collection[index] = null;
+    }
+  });
 };
 
 /*
