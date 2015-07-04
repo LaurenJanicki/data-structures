1d19e2ea5de409470bbfdf10baf23cb2df29b73d
LaurenJanicki <lauren.a.janicki@gmail.com>
2015-07-04 16:31:27 -0700

    Binary Search Tree insert and contains methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,54 @@
 var BinarySearchTree = function(value){
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  tree.insert = function(value) {
+    if (value < tree.value) {
+      if (!tree.left) {
+        tree.left = BinarySearchTree(value);
+      } else {
+        tree.left.insert(value);
+      }
+    } else {
+      if (!tree.right) {
+        tree.right = BinarySearchTree(value);
+      } else {
+        tree.right.insert(value);
+      }
+    }
+
+  };
+
+  tree.contains = function(target) {
+    var found = false;
+    if (tree.value === target) {
+      found = true;
+    } else if (target < tree.value) {
+      if (tree.left) {
+        return tree.left.contains(target);
+      } 
+    } else if (target > tree.value) {
+      if (tree.right) {
+        return tree.right.contains(target);
+      }
+    }
+    console.log('' + target + found);
+    return found;
+  };
+
+  tree.depthFirstLog = function() {
+
+  };
+
+
+
+
+  return tree;
+
+
+
 };
 
 
